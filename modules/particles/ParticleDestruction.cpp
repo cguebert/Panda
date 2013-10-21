6603bf9d9e65d87bae53163a97eb506f64782244
Christophe Guébert <christophe.guebert@gmail.com>
2013-10-21 20:39:42 +0000

    Ajout de l'objet ParticleDestructionByIndex.

--- modules/particles/ParticleDestruction.cpp
+++ modules/particles/ParticleDestruction.cpp
@@ -71,6 +71,35 @@ protected:
 int ParticleDestruction_AgeClass = RegisterObject<ParticleDestruction_Age>("Particles/Destruction/Remove old particles")
 		.setDescription("Remove particles older than a given age");
 
+//*************************************************************************//
+
+class ParticleDestruction_Index : public ParticleEffector
+{
+public:
+	PANDA_CLASS(ParticleDestruction_Index, ParticleEffector)
+
+	ParticleDestruction_Index(PandaDocument *doc)
+		: ParticleEffector(doc)
+		, indices(initData(&indices, "indices", "Remove these particles"))
+	{
+		addInput(&indices);
+	}
+
+	virtual Indices filterParticles(const Particles&)
+	{
+		Indices tmp;
+		auto ind = indices.getValue();
+		tmp.insert(ind.begin(), ind.end());
+
+		return tmp;
+	}
+
+protected:
+	Data< QVector<int> > indices;
+};
+
+int ParticleDestruction_IndexClass = RegisterObject<ParticleDestruction_Index>("Particles/Destruction/Remove particles by index")
+		.setDescription("Remove specific particles based on their index");
 
 } // namespace Panda
 
