3a21e474c2bc922028471b36ef8218a6bc53c27b
Christophe Guébert <christophe.guebert@gmail.com>
2014-02-25 22:42:07 +0000

    Ajout de l'objet SeparateDisconnected.

--- modules/modifiers/topology/Neighbors.cpp
+++ modules/modifiers/topology/Neighbors.cpp
@@ -104,5 +104,62 @@ protected:
 
 int ModifierTopology_GetConnectedClass = RegisterObject<ModifierTopology_GetConnected>("Modifier/Topology/Get connected").setDescription("Get connected polygons to the input list");
 
+//*************************************************************************//
+
+class ModifierTopology_SeparateDisconnected : public PandaObject
+{
+public:
+	PANDA_CLASS(ModifierTopology_SeparateDisconnected, PandaObject)
+
+	ModifierTopology_SeparateDisconnected(PandaDocument *doc)
+		: PandaObject(doc)
+		, input(initData(&input, "input", "Input topology"))
+		, outputs(initData(&outputs, "output", "List of separated topologies"))
+	{
+		addInput(&input);
+
+		addOutput(&outputs);
+	}
+
+	void update()
+	{
+		Topology topo = input.getValue();
+
+		auto topoList = outputs.getAccessor();
+		topoList.clear();
+
+		std::set<Topology::PolygonID> polySet;
+		for(int i=0, nb=topo.getNumberOfPolygons(); i<nb; ++i)
+			polySet.insert(i);
+
+		while(!polySet.empty())
+		{
+			Topology::PolygonID polyID = *polySet.begin();
+			Topology newTopo;
+			newTopo.addPoints(topo.getPoints());
+			newTopo.addPolygon(topo.getPolygon(polyID));
+			auto list = topo.getPolygonsConnectedToPolygon(polyID);
+
+			for(auto i : list)
+			{
+				newTopo.addPolygon(topo.getPolygon(i));
+				polySet.erase(i);
+			}
+
+			newTopo.removeUnusedPoints();
+			topoList.push_back(newTopo);
+		}
+
+		cleanDirty();
+	}
+
+protected:
+	Data< Topology > input;
+	Data< QVector<Topology> > outputs;
+};
+
+int ModifierTopology_SeparateDisconnectedClass = RegisterObject<ModifierTopology_SeparateDisconnected>("Modifier/Topology/Separate disconnected")
+		.setDescription("Separate the disconnected parts of a topology into many topologies");
+
 
 } // namespace Panda
