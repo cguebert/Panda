672ab20088bd6b8131aa517aeae16fa8c1f762e5
Christophe Guébert <christophe.guebert@gmail.com>
2014-03-08 16:50:22 +0000

    Utilisation d'un widget enum pour la Data "mode" des objets Interpolation.

--- modules/animation/Interpolation.cpp
+++ modules/animation/Interpolation.cpp
@@ -28,6 +28,20 @@ public:
 		addInput(&mode);
 
 		addOutput(&result);
+
+		mode.setWidget("enum");
+		mode.setWidgetData("Linear"
+									";InQuad;OutQuad;InOutQuad;OutInQuad"
+									";InCubic;OutCubic;InOutCubic;OutInCubic"
+									";InQuart;OutQuart;InOutQuart;OutInQuart"
+									";InQuint;OutQuint;InOutQuint;OutInQuint"
+									";InSine;OutSine;InOutSine;OutInSine"
+									";InExpo;OutExpo;InOutExpo;OutInExpo"
+									";InCirc;OutCirc;InOutCirc;OutInCirc"
+									";InElastic;OutElastic;InOutElastic;OutInElastic"
+									";InBack;OutBack;InOutBack;OutInBack"
+									";InBounce;OutBounce;InOutBounce;OutInBounce"
+									";InCurve;OutCurve;SineCurve;CosineCurve");
 	}
 
 	void update()
