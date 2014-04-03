8426f88438c0d2564fff7e93c8231b0b2018717e
Christophe Guébert <christophe.guebert@gmail.com>
2014-04-03 20:14:32 +0000

    Ajout des objets composeHSV et decomposeHSV.

--- modules/generators/color/ComposeColors.cpp
+++ modules/generators/color/ComposeColors.cpp
@@ -22,7 +22,7 @@ public:
 		addInput(&B);
 		addInput(&A);
 
-		A.getAccessor().push_back(0.0);
+		A.getAccessor().push_back(1.0);
 
 		addOutput(&color);
 	}
@@ -33,14 +33,22 @@ public:
 		const QVector<PReal> &g = G.getValue();
 		const QVector<PReal> &b = B.getValue();
 		const QVector<PReal> &a = A.getValue();
+		auto c = color.getAccessor();
 
 		int nbR = r.size(), nbG = g.size(), nbB = b.size(), nbA = a.size();
+		if(!nbR || !nbG || !nbB || !nbA)
+		{
+			c.clear();
+			cleanDirty();
+			return;
+		}
+
 		int nb = qMax(nbR, qMax(nbG, qMax(nbB, nbA)));
 		if(nbR < nb) nbR = 1;
 		if(nbG < nb) nbG = 1;
 		if(nbB < nb) nbB = 1;
 		if(nbA < nb) nbA = 1;
-		auto c = color.getAccessor();
+
 		c.resize(nb);
 		for(int i=0; i<nb; ++i)
 		{
@@ -114,6 +122,127 @@ protected:
 };
 
 int GeneratorColors_DecomposeRGBClass = RegisterObject<GeneratorColors_DecomposeRGB>("Generator/Color/To RGB").setDescription("Extract red, green and blue components from a color");
+
+//*************************************************************************//
+
+class GeneratorColors_ComposeHSV : public PandaObject
+{
+public:
+	PANDA_CLASS(GeneratorColors_ComposeHSV, PandaObject)
+
+	GeneratorColors_ComposeHSV(PandaDocument *doc)
+		: PandaObject(doc)
+		, H(initData(&H, "h", "Hue component of the color"))
+		, S(initData(&S, "s", "Saturation component of the color"))
+		, V(initData(&V, "v", "Value component of the color"))
+		, A(initData(&A, "a", "Alpha component of the color"))
+		, color(initData(&color, "color", "Color created from the 4 components"))
+	{
+		addInput(&H);
+		addInput(&S);
+		addInput(&V);
+		addInput(&A);
+
+		A.getAccessor().push_back(1.0);
+
+		addOutput(&color);
+	}
+
+	void update()
+	{
+		const QVector<PReal> &h = H.getValue();
+		const QVector<PReal> &s = S.getValue();
+		const QVector<PReal> &v = V.getValue();
+		const QVector<PReal> &a = A.getValue();
+		auto c = color.getAccessor();
+
+		int nbH = h.size(), nbS = s.size(), nbV = v.size(), nbA = a.size();
+		if(!nbH || !nbS || !nbV || !nbA)
+		{
+			c.clear();
+			cleanDirty();
+			return;
+		}
+
+		int nb = qMax(nbH, qMax(nbS, qMax(nbV, nbA)));
+		if(nbH < nb) nbH = 1;
+		if(nbS < nb) nbS = 1;
+		if(nbV < nb) nbV = 1;
+		if(nbA < nb) nbA = 1;
+
+		c.resize(nb);
+		for(int i=0; i<nb; ++i)
+		{
+			c[i].setHsvF(qBound<PReal>(0.0, h[i%nbH], 1.0),
+						 qBound<PReal>(0.0, s[i%nbS], 1.0),
+						 qBound<PReal>(0.0, v[i%nbV], 1.0),
+						 qBound<PReal>(0.0, a[i%nbA], 1.0));
+		}
+
+		cleanDirty();
+	}
+
+protected:
+	Data< QVector<PReal> > H, S, V, A;
+	Data< QVector<QColor> > color;
+};
+
+int GeneratorColors_ComposeHSVClass = RegisterObject<GeneratorColors_ComposeHSV>("Generator/Color/From HSV").setDescription("Create a color from hue, saturation and value components");
+
+//*************************************************************************//
+
+class GeneratorColors_DecomposeHSV : public PandaObject
+{
+public:
+	PANDA_CLASS(GeneratorColors_DecomposeHSV, PandaObject)
+
+	GeneratorColors_DecomposeHSV(PandaDocument *doc)
+		: PandaObject(doc)
+		, H(initData(&H, "h", "Hue component of the color"))
+		, S(initData(&S, "s", "Saturation component of the color"))
+		, V(initData(&V, "v", "Value component of the color"))
+		, A(initData(&A, "a", "Alpha component of the color"))
+		, color(initData(&color, "color", "Color created from the 4 components"))
+	{
+		addInput(&color);
+
+		addOutput(&H);
+		addOutput(&S);
+		addOutput(&V);
+		addOutput(&A);
+	}
+
+	void update()
+	{
+		const QVector<QColor>& c = color.getValue();
+		auto h = H.getAccessor();
+		auto s = S.getAccessor();
+		auto v = V.getAccessor();
+		auto a = A.getAccessor();
+
+		int nb = c.size();
+		h.resize(nb);
+		s.resize(nb);
+		v.resize(nb);
+		a.resize(nb);
+
+		for(int i=0; i<nb; ++i)
+		{
+			h[i] = c[i].hsvHueF();
+			s[i] = c[i].hsvSaturationF();
+			v[i] = c[i].valueF();
+			a[i] = c[i].alphaF();
+		}
+
+		cleanDirty();
+	}
+
+protected:
+	Data< QVector<PReal> > H, S, V, A;
+	Data< QVector<QColor> > color;
+};
+
+int GeneratorColors_DecomposeHSVClass = RegisterObject<GeneratorColors_DecomposeHSV>("Generator/Color/To HSV").setDescription("Extract hue, saturation and value components from a color");
 
 
 } // namespace Panda
