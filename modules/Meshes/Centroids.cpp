0c1d8a831ae7a384194475d7a96bb0c05b5196df
Christophe Guébert <christophe.guebert@gmail.com>
2015-01-26 10:44:36 +0000

    Ajout de l'objet MeshCentroid.

--- modules/Meshes/Centroids.cpp
+++ modules/Meshes/Centroids.cpp
@@ -16,18 +16,17 @@ public:
 
 	MeshMath_Centroids(PandaDocument *doc)
 		: PandaObject(doc)
-		, mesh(initData("mesh", "Mesh to analyse"))
-		, centroids(initData("centroids", "Centroids of the triangles"))
+		, m_mesh(initData("mesh", "Mesh to analyse"))
+		, m_centroids(initData("centroids", "Centroids of the triangles"))
 	{
-		addInput(mesh);
-
-		addOutput(centroids);
+		addInput(m_mesh);
+		addOutput(m_centroids);
 	}
 
 	void update()
 	{
-		const Mesh& inMesh = mesh.getValue();
-		auto pts = centroids.getAccessor();
+		const Mesh& inMesh = m_mesh.getValue();
+		auto pts = m_centroids.getAccessor();
 		pts.clear();
 		pts.resize(inMesh.nbTriangles());
 
@@ -39,11 +38,63 @@ public:
 	}
 
 protected:
-	Data<Mesh> mesh;
-	Data< QVector<Point> > centroids;
+	Data<Mesh> m_mesh;
+	Data< QVector<Point> > m_centroids;
 };
 
-int MeshMath_CentroidsClass = RegisterObject<MeshMath_Centroids>("Math/Mesh/Centroids")
+int MeshMath_CentroidsClass = RegisterObject<MeshMath_Centroids>("Math/Mesh/Centroids of triangles")
 		.setDescription("Compute the centroid of each triangle");
+
+//****************************************************************************//
+
+class MeshMath_Centroid : public PandaObject
+{
+public:
+	PANDA_CLASS(MeshMath_Centroid, PandaObject)
+
+	MeshMath_Centroid(PandaDocument *doc)
+		: PandaObject(doc)
+		, m_mesh(initData("mesh", "Mesh to analyse"))
+		, m_centroids(initData("centroids", "Centroids of the triangles"))
+	{
+		addInput(m_mesh);
+		addOutput(m_centroids);
+	}
+
+	void update()
+	{
+		const auto& meshes = m_mesh.getValue();
+		auto centroids = m_centroids.getAccessor();
+		centroids.clear();
+		int nb = meshes.size();
+		centroids.resize(nb);
+
+		for(int i=0; i<nb; ++i)
+		{
+			Point avg;
+			PReal totArea = 0;
+			const auto& mesh = meshes[i];
+			for(int j=0, nbP = mesh.nbTriangles(); j<nbP; ++j)
+			{
+				const auto tri = mesh.getTriangle(j);
+				PReal area = mesh.areaOfTriangle(tri);
+				avg += mesh.centroidOfTriangle(tri) * area;
+				totArea += area;
+			}
+
+			centroids[i] = avg / totArea;
+		}
+
+		cleanDirty();
+	}
+
+protected:
+	Data< QVector<Mesh> > m_mesh;
+	Data< QVector<Point> > m_centroids;
+};
+
+int MeshMath_CentroidClass = RegisterObject<MeshMath_Centroid>("Math/Mesh/Centroid")
+		.setName("Centroid of mesh")
+		.setDescription("Compute the centroid of a mesh");
 
 } // namespace Panda
