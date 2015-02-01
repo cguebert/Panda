793f6be0420634d9118be5e370dad87b0e4ca6ba
Christophe Guébert <christophe.guebert@gmail.com>
2015-02-01 22:39:32 +0000

    Extrude transforme maintenant une liste de Path en une liste de Mesh. (Les coordonnées de texture sont exportées sous une liste de liste de points).

--- modules/Paths/Extrude.cpp
+++ modules/Paths/Extrude.cpp
@@ -21,40 +21,35 @@ public:
 
 	PointListMath_Extrude(PandaDocument *doc)
 		: PandaObject(doc)
-		, input(initData("input", "List of control points"))		
-		, width(initData("width", "Width of the line"))
-		, capStyle(initData("cap", "Style of the caps"))
-		, joinStyle(initData("join", "Style of the joins"))
-		, output(initData("output", "Mesh created from the extrusion"))
-		, coordUV(initData("UV coords", "UV coords of the points in the mesh"))
+		, m_input(initData("input", "List of control points"))
+		, m_width(initData("width", "Width of the line"))
+		, m_capStyle(initData("cap", "Style of the caps"))
+		, m_joinStyle(initData("join", "Style of the joins"))
+		, m_output(initData("output", "Mesh created from the extrusion"))
+		, m_coordUV(initData("UV coords", "UV coords of the points in the mesh"))
 	{
-		addInput(input);
-		addInput(width);
-		addInput(capStyle);
-		addInput(joinStyle);
+		addInput(m_input);
+		addInput(m_width);
+		addInput(m_capStyle);
+		addInput(m_joinStyle);
 
-		capStyle.setWidget("enum");
-		capStyle.setWidgetData("Butt cap;Round cap;Square cap");
-		joinStyle.setWidget("enum");
-		joinStyle.setWidgetData("Miter join;Round join;Bevel join");
+		m_capStyle.setWidget("enum");
+		m_capStyle.setWidgetData("Butt cap;Round cap;Square cap");
+		m_joinStyle.setWidget("enum");
+		m_joinStyle.setWidgetData("Miter join;Round join;Bevel join");
 
-		addOutput(output);
-		addOutput(coordUV);
+		addOutput(m_output);
+		addOutput(m_coordUV);
 
-		width.getAccessor()->add(0, 10.0);
+		m_width.getAccessor()->add(0, 10.0);
 	}
 
-	void update()
+	void extrudePath(const Path& pts, Mesh& mesh, Path& UV)
 	{
-		const QVector<Point>& pts = input.getValue();
-		const Animation<PReal>& widthAnim = width.getValue();
+		const Animation<PReal>& widthAnim = m_width.getValue();
 		PReal w = widthAnim.get(0) / 2;
-		int cap = capStyle.getValue();
-		int join = joinStyle.getValue();
-		auto mesh = output.getAccessor();
-		auto UV = coordUV.getAccessor();
-		mesh->clear();
-		UV.clear();
+		int cap = m_capStyle.getValue();
+		int join = m_joinStyle.getValue();
 
 		int nbPts = pts.size();
 		if(!nbPts || !widthAnim.size())
@@ -84,55 +79,55 @@ public:
 		// Start cap
 		Mesh::PointID prevPtsId[3];
 		Point dir = normals[0] * w;
-		prevPtsId[0] = mesh->addPoint(pts[0] + dir);
-		prevPtsId[1] = mesh->addPoint(pts[0]);
-		prevPtsId[2] = mesh->addPoint(pts[0] - dir);
+		prevPtsId[0] = mesh.addPoint(pts[0] + dir);
+		prevPtsId[1] = mesh.addPoint(pts[0]);
+		prevPtsId[2] = mesh.addPoint(pts[0] - dir);
 		UV.push_back(Point(abscissa[0], 1));
 		UV.push_back(Point(abscissa[0], 0.5));
 		UV.push_back(Point(abscissa[0], 0));
 		switch(cap)
 		{
-		default:
-		case 0:	// Butt cap (nothing to do)
-			break;
-		case 1:	// Round cap
-		{
-			int nb = static_cast<int>(floor(w * M_PI));
-			PReal angle = M_PI / nb; // We do a half turn
-			PReal ca = cos(angle), sa = sin(angle);
-			Point center = pts[0];
-			Point uvPt(0, 1), uvCenter(abscissa[0], 0.5), uvMult(-abscissa[0], 0.5);
-			Mesh::PointID ptId = prevPtsId[0];
-			for(int i=0; i<nb-1; ++i)
+			default:
+			case 0:	// Butt cap (nothing to do)
+				break;
+			case 1:	// Round cap
 			{
-				dir = Point(dir.x*ca+dir.y*sa, dir.y*ca-dir.x*sa);
-				uvPt = Point(uvPt.x*ca+uvPt.y*sa, uvPt.y*ca-uvPt.x*sa);
-				Mesh::PointID newPtId = mesh->addPoint(center + dir);
-				UV.push_back(uvCenter + uvPt.linearProduct(uvMult));
-				mesh->addTriangle(ptId, newPtId, prevPtsId[1]);
+				int nb = static_cast<int>(floor(w * M_PI));
+				PReal angle = M_PI / nb; // We do a half turn
+				PReal ca = cos(angle), sa = sin(angle);
+				Point center = pts[0];
+				Point uvPt(0, 1), uvCenter(abscissa[0], 0.5), uvMult(-abscissa[0], 0.5);
+				Mesh::PointID ptId = prevPtsId[0];
+				for(int i=0; i<nb-1; ++i)
+				{
+					dir = Point(dir.x*ca+dir.y*sa, dir.y*ca-dir.x*sa);
+					uvPt = Point(uvPt.x*ca+uvPt.y*sa, uvPt.y*ca-uvPt.x*sa);
+					Mesh::PointID newPtId = mesh.addPoint(center + dir);
+					UV.push_back(uvCenter + uvPt.linearProduct(uvMult));
+					mesh.addTriangle(ptId, newPtId, prevPtsId[1]);
 
-				ptId = newPtId;
+					ptId = newPtId;
+				}
+				mesh.addTriangle(ptId, prevPtsId[2], prevPtsId[1]);
+				break;
 			}
-			mesh->addTriangle(ptId, prevPtsId[2], prevPtsId[1]);
-			break;
-		}
-		case 2: // Square cap
-		{
-			Point dir2 = Point(dir.y, -dir.x);
-			Mesh::PointID addPtsId[3];
-			addPtsId[0] = mesh->addPoint(pts[0] + dir + dir2);
-			addPtsId[1] = mesh->addPoint(pts[0]	      + dir2);
-			addPtsId[2] = mesh->addPoint(pts[0] - dir + dir2);
-			UV.push_back(Point(0, 1));
-			UV.push_back(Point(0, 0.5));
-			UV.push_back(Point(0, 0));
+			case 2: // Square cap
+			{
+				Point dir2 = Point(dir.y, -dir.x);
+				Mesh::PointID addPtsId[3];
+				addPtsId[0] = mesh.addPoint(pts[0] + dir + dir2);
+				addPtsId[1] = mesh.addPoint(pts[0]	      + dir2);
+				addPtsId[2] = mesh.addPoint(pts[0] - dir + dir2);
+				UV.push_back(Point(0, 1));
+				UV.push_back(Point(0, 0.5));
+				UV.push_back(Point(0, 0));
 
-			mesh->addTriangle(prevPtsId[0], addPtsId[0], prevPtsId[1]);
-			mesh->addTriangle(prevPtsId[1], addPtsId[0], addPtsId[1]);
-			mesh->addTriangle(addPtsId[1], addPtsId[2], prevPtsId[1]);
-			mesh->addTriangle(prevPtsId[1], addPtsId[2], prevPtsId[2]);
-			break;
-		}
+				mesh.addTriangle(prevPtsId[0], addPtsId[0], prevPtsId[1]);
+				mesh.addTriangle(prevPtsId[1], addPtsId[0], addPtsId[1]);
+				mesh.addTriangle(addPtsId[1], addPtsId[2], prevPtsId[1]);
+				mesh.addTriangle(prevPtsId[1], addPtsId[2], prevPtsId[2]);
+				break;
+			}
 		} // end switch
 
 		// Line
@@ -157,25 +152,25 @@ public:
 				hasInteriorPt = false;	// I don't know what to do in this degenerated case!
 
 			// Main extrusion (without the exterior of the curvature)
-			nextPtsId[1] = mesh->addPoint(pts[i]);
+			nextPtsId[1] = mesh.addPoint(pts[i]);
 			UV.push_back(Point(abscissa[i], 0.5));
-			mesh->addTriangle(nextPtsId[1], prevPtsId[0], prevPtsId[1]);
-			mesh->addTriangle(nextPtsId[1], prevPtsId[1], prevPtsId[2]);
+			mesh.addTriangle(nextPtsId[1], prevPtsId[0], prevPtsId[1]);
+			mesh.addTriangle(nextPtsId[1], prevPtsId[1], prevPtsId[2]);
 
 			if(side > 0)
 			{
 				if(hasInteriorPt)
 				{
-					nextPtsId[2] = mesh->addPoint(pts[i] - dir);
+					nextPtsId[2] = mesh.addPoint(pts[i] - dir);
 					UV.push_back(Point(abscissa[i], 0));
-					mesh->addTriangle(nextPtsId[1], prevPtsId[2], nextPtsId[2]);
+					mesh.addTriangle(nextPtsId[1], prevPtsId[2], nextPtsId[2]);
 				}
 				else
 				{
-					Mesh::PointID addPtId = mesh->addPoint(pts[i] - normals[i-1] * w);
+					Mesh::PointID addPtId = mesh.addPoint(pts[i] - normals[i-1] * w);
 					UV.push_back(Point(abscissa[i], 0));
-					mesh->addTriangle(nextPtsId[1], prevPtsId[2], addPtId);
-					nextPtsId[2] = mesh->addPoint(pts[i] - normals[i] * w);
+					mesh.addTriangle(nextPtsId[1], prevPtsId[2], addPtId);
+					nextPtsId[2] = mesh.addPoint(pts[i] - normals[i] * w);
 					UV.push_back(Point(abscissa[i], 0));
 				}
 
@@ -184,16 +179,16 @@ public:
 			{
 				if(hasInteriorPt)
 				{
-					nextPtsId[0] = mesh->addPoint(pts[i] + dir);
+					nextPtsId[0] = mesh.addPoint(pts[i] + dir);
 					UV.push_back(Point(abscissa[i], 1));
-					mesh->addTriangle(nextPtsId[0], prevPtsId[0], nextPtsId[1]);
+					mesh.addTriangle(nextPtsId[0], prevPtsId[0], nextPtsId[1]);
 				}
 				else
 				{
-					Mesh::PointID addPtId = mesh->addPoint(pts[i] + normals[i-1] * w);
+					Mesh::PointID addPtId = mesh.addPoint(pts[i] + normals[i-1] * w);
 					UV.push_back(Point(abscissa[i], 1));
-					mesh->addTriangle(addPtId, prevPtsId[0], nextPtsId[1]);
-					nextPtsId[0] = mesh->addPoint(pts[i] + normals[i] * w);
+					mesh.addTriangle(addPtId, prevPtsId[0], nextPtsId[1]);
+					nextPtsId[0] = mesh.addPoint(pts[i] + normals[i] * w);
 					UV.push_back(Point(abscissa[i], 1));
 				}
 			}
@@ -206,108 +201,108 @@ public:
 			// Join
 			switch(tmpJoin)
 			{
-			default:
-			case 0: // Miter join
-			{
-				if(side > 0)
+				default:
+				case 0: // Miter join
 				{
-					nextPtsId[0] = mesh->addPoint(pts[i] + dir);
-					UV.push_back(Point(abscissa[i], 1));
-					mesh->addTriangle(nextPtsId[0], prevPtsId[0], nextPtsId[1]);
-				}
-				else // side < 0
-				{
-					nextPtsId[2] = mesh->addPoint(pts[i] - dir);
-					UV.push_back(Point(abscissa[i], 0));
-					mesh->addTriangle(nextPtsId[2], nextPtsId[1], prevPtsId[2]);
-				}
-
-				break;
-			}
-			case 1: // Round join
-			{
-				if(side > 0)
-				{
-					PReal angle = acos(normals[i-1].dot(normals[i]));
-					Mesh::PointID addPtId = mesh->addPoint(pts[i] + normals[i-1] * w);
-					nextPtsId[0] = mesh->addPoint(pts[i] + normals[i] * w);
-					UV.push_back(Point(abscissa[i], 1));
-					UV.push_back(Point(abscissa[i], 1));
-
-					mesh->addTriangle(addPtId, prevPtsId[0], nextPtsId[1]);
-
-					int nb = static_cast<int>(floor(w * angle));
-					angle /= nb;
-					PReal ca = cos(angle), sa = sin(angle);
-					Point center = pts[i];
-					Point r = normals[i] * w;
-					Mesh::PointID ptId = nextPtsId[0];
-					for(int j=0; j<nb-1; ++j)
+					if(side > 0)
 					{
-						Point nr = Point(r.x*ca+r.y*sa, r.y*ca-r.x*sa);
-						Mesh::PointID newPtId = mesh->addPoint(center + nr);
+						nextPtsId[0] = mesh.addPoint(pts[i] + dir);
 						UV.push_back(Point(abscissa[i], 1));
-						mesh->addTriangle(ptId, newPtId, nextPtsId[1]);
-
-						r = nr;
-						ptId = newPtId;
+						mesh.addTriangle(nextPtsId[0], prevPtsId[0], nextPtsId[1]);
 					}
-					mesh->addTriangle(ptId, addPtId, nextPtsId[1]);
-				}
-				else // side < 0
-				{
-					PReal angle = acos(normals[i-1].dot(normals[i]));
-					nextPtsId[2] = mesh->addPoint(pts[i] - normals[i] * w);
-					Mesh::PointID addPtId = mesh->addPoint(pts[i] - normals[i-1] * w);
-					UV.push_back(Point(abscissa[i], 0));
-					UV.push_back(Point(abscissa[i], 0));
-
-					mesh->addTriangle(addPtId, nextPtsId[1], prevPtsId[2]);
-
-					int nb = static_cast<int>(floor(w * angle));
-					angle /= nb;
-					PReal ca = cos(angle), sa = sin(angle);
-					Point center = pts[i];
-					Point r = normals[i-1] * w;
-					Mesh::PointID ptId = addPtId;
-					for(int j=0; j<nb-1; ++j)
+					else // side < 0
 					{
-						Point nr = Point(r.x*ca+r.y*sa, r.y*ca-r.x*sa);
-						Mesh::PointID newPtId = mesh->addPoint(center - nr);
+						nextPtsId[2] = mesh.addPoint(pts[i] - dir);
 						UV.push_back(Point(abscissa[i], 0));
-						mesh->addTriangle(ptId, newPtId, nextPtsId[1]);
-
-						r = nr;
-						ptId = newPtId;
+						mesh.addTriangle(nextPtsId[2], nextPtsId[1], prevPtsId[2]);
 					}
-					mesh->addTriangle(ptId, nextPtsId[2], nextPtsId[1]);
-				}
-				break;
-			}
-			case 2: // Bevel join
-			{
-				if(side > 0)
-				{
-					Mesh::PointID addPtId = mesh->addPoint(pts[i] + normals[i-1] * w);
-					nextPtsId[0] = mesh->addPoint(pts[i] + normals[i] * w);
-					UV.push_back(Point(abscissa[i], 1));
-					UV.push_back(Point(abscissa[i], 1));
 
-					mesh->addTriangle(nextPtsId[0], addPtId, nextPtsId[1]);
-					mesh->addTriangle(addPtId, prevPtsId[0], nextPtsId[1]);
+					break;
 				}
-				else // side < 0
+				case 1: // Round join
 				{
-					nextPtsId[2] = mesh->addPoint(pts[i] - normals[i] * w);
-					Mesh::PointID addPtId = mesh->addPoint(pts[i] - normals[i-1] * w);
-					UV.push_back(Point(abscissa[i], 0));
-					UV.push_back(Point(abscissa[i], 0));
+					if(side > 0)
+					{
+						PReal angle = acos(normals[i-1].dot(normals[i]));
+						Mesh::PointID addPtId = mesh.addPoint(pts[i] + normals[i-1] * w);
+						nextPtsId[0] = mesh.addPoint(pts[i] + normals[i] * w);
+						UV.push_back(Point(abscissa[i], 1));
+						UV.push_back(Point(abscissa[i], 1));
 
-					mesh->addTriangle(nextPtsId[2], nextPtsId[1], addPtId);
-					mesh->addTriangle(addPtId, nextPtsId[1], prevPtsId[2]);
+						mesh.addTriangle(addPtId, prevPtsId[0], nextPtsId[1]);
+
+						int nb = static_cast<int>(floor(w * angle));
+						angle /= nb;
+						PReal ca = cos(angle), sa = sin(angle);
+						Point center = pts[i];
+						Point r = normals[i] * w;
+						Mesh::PointID ptId = nextPtsId[0];
+						for(int j=0; j<nb-1; ++j)
+						{
+							Point nr = Point(r.x*ca+r.y*sa, r.y*ca-r.x*sa);
+							Mesh::PointID newPtId = mesh.addPoint(center + nr);
+							UV.push_back(Point(abscissa[i], 1));
+							mesh.addTriangle(ptId, newPtId, nextPtsId[1]);
+
+							r = nr;
+							ptId = newPtId;
+						}
+						mesh.addTriangle(ptId, addPtId, nextPtsId[1]);
+					}
+					else // side < 0
+					{
+						PReal angle = acos(normals[i-1].dot(normals[i]));
+						nextPtsId[2] = mesh.addPoint(pts[i] - normals[i] * w);
+						Mesh::PointID addPtId = mesh.addPoint(pts[i] - normals[i-1] * w);
+						UV.push_back(Point(abscissa[i], 0));
+						UV.push_back(Point(abscissa[i], 0));
+
+						mesh.addTriangle(addPtId, nextPtsId[1], prevPtsId[2]);
+
+						int nb = static_cast<int>(floor(w * angle));
+						angle /= nb;
+						PReal ca = cos(angle), sa = sin(angle);
+						Point center = pts[i];
+						Point r = normals[i-1] * w;
+						Mesh::PointID ptId = addPtId;
+						for(int j=0; j<nb-1; ++j)
+						{
+							Point nr = Point(r.x*ca+r.y*sa, r.y*ca-r.x*sa);
+							Mesh::PointID newPtId = mesh.addPoint(center - nr);
+							UV.push_back(Point(abscissa[i], 0));
+							mesh.addTriangle(ptId, newPtId, nextPtsId[1]);
+
+							r = nr;
+							ptId = newPtId;
+						}
+						mesh.addTriangle(ptId, nextPtsId[2], nextPtsId[1]);
+					}
+					break;
 				}
-				break;
-			}
+				case 2: // Bevel join
+				{
+					if(side > 0)
+					{
+						Mesh::PointID addPtId = mesh.addPoint(pts[i] + normals[i-1] * w);
+						nextPtsId[0] = mesh.addPoint(pts[i] + normals[i] * w);
+						UV.push_back(Point(abscissa[i], 1));
+						UV.push_back(Point(abscissa[i], 1));
+
+						mesh.addTriangle(nextPtsId[0], addPtId, nextPtsId[1]);
+						mesh.addTriangle(addPtId, prevPtsId[0], nextPtsId[1]);
+					}
+					else // side < 0
+					{
+						nextPtsId[2] = mesh.addPoint(pts[i] - normals[i] * w);
+						Mesh::PointID addPtId = mesh.addPoint(pts[i] - normals[i-1] * w);
+						UV.push_back(Point(abscissa[i], 0));
+						UV.push_back(Point(abscissa[i], 0));
+
+						mesh.addTriangle(nextPtsId[2], nextPtsId[1], addPtId);
+						mesh.addTriangle(addPtId, nextPtsId[1], prevPtsId[2]);
+					}
+					break;
+				}
 			} // end switch
 
 			for(int j=0; j<3; ++j)
@@ -318,73 +313,90 @@ public:
 		w = widthAnim.get(abscissa[nbPts-1]) / 2;
 		Mesh::PointID nextPtsId[3];
 		dir = normals[nbPts-2] * w;
-		nextPtsId[0] = mesh->addPoint(pts[nbPts-1] + dir);
-		nextPtsId[1] = mesh->addPoint(pts[nbPts-1]);
-		nextPtsId[2] = mesh->addPoint(pts[nbPts-1] - dir);
+		nextPtsId[0] = mesh.addPoint(pts[nbPts-1] + dir);
+		nextPtsId[1] = mesh.addPoint(pts[nbPts-1]);
+		nextPtsId[2] = mesh.addPoint(pts[nbPts-1] - dir);
 		UV.push_back(Point(abscissa[nbPts-1], 1));
 		UV.push_back(Point(abscissa[nbPts-1], 0.5));
 		UV.push_back(Point(abscissa[nbPts-1], 0));
-		mesh->addTriangle(nextPtsId[0], prevPtsId[0], nextPtsId[1]);
-		mesh->addTriangle(prevPtsId[0], prevPtsId[1], nextPtsId[1]);
-		mesh->addTriangle(nextPtsId[1], prevPtsId[1], prevPtsId[2]);
-		mesh->addTriangle(nextPtsId[1], prevPtsId[2], nextPtsId[2]);
+		mesh.addTriangle(nextPtsId[0], prevPtsId[0], nextPtsId[1]);
+		mesh.addTriangle(prevPtsId[0], prevPtsId[1], nextPtsId[1]);
+		mesh.addTriangle(nextPtsId[1], prevPtsId[1], prevPtsId[2]);
+		mesh.addTriangle(nextPtsId[1], prevPtsId[2], nextPtsId[2]);
 
 		// End cap
 		switch(cap)
 		{
-		default:
-		case 0:	// Butt cap (nothing to do)
-			break;
-		case 1:	// Round cap
-		{
-			int nb = static_cast<int>(floor(w * M_PI));
-			PReal angle = M_PI / nb; // We do a half turn
-			PReal ca = cos(angle), sa = sin(angle);
-			Point center = pts[nbPts-1];
-			dir = -dir;
-			Mesh::PointID ptId = nextPtsId[2];
-			Point uvPt(0, 1), uvCenter(abscissa[nbPts-1], 0.5), uvMult(abscissa[0], -0.5);
-			for(int i=0; i<nb-1; ++i)
+			default:
+			case 0:	// Butt cap (nothing to do)
+				break;
+			case 1:	// Round cap
 			{
-				dir = Point(dir.x*ca+dir.y*sa, dir.y*ca-dir.x*sa);
-				uvPt = Point(uvPt.x*ca+uvPt.y*sa, uvPt.y*ca-uvPt.x*sa);
-				Mesh::PointID newPtId = mesh->addPoint(center + dir);
-				UV.push_back(uvCenter + uvPt.linearProduct(uvMult));
-				mesh->addTriangle(ptId, newPtId, nextPtsId[1]);
+				int nb = static_cast<int>(floor(w * M_PI));
+				PReal angle = M_PI / nb; // We do a half turn
+				PReal ca = cos(angle), sa = sin(angle);
+				Point center = pts[nbPts-1];
+				dir = -dir;
+				Mesh::PointID ptId = nextPtsId[2];
+				Point uvPt(0, 1), uvCenter(abscissa[nbPts-1], 0.5), uvMult(abscissa[0], -0.5);
+				for(int i=0; i<nb-1; ++i)
+				{
+					dir = Point(dir.x*ca+dir.y*sa, dir.y*ca-dir.x*sa);
+					uvPt = Point(uvPt.x*ca+uvPt.y*sa, uvPt.y*ca-uvPt.x*sa);
+					Mesh::PointID newPtId = mesh.addPoint(center + dir);
+					UV.push_back(uvCenter + uvPt.linearProduct(uvMult));
+					mesh.addTriangle(ptId, newPtId, nextPtsId[1]);
 
-				ptId = newPtId;
+					ptId = newPtId;
+				}
+				mesh.addTriangle(ptId, nextPtsId[0], nextPtsId[1]);
+				break;
 			}
-			mesh->addTriangle(ptId, nextPtsId[0], nextPtsId[1]);
-			break;
-		}
-		case 2: // Square cap
-		{
-			Point dir2 = Point(dir.y, -dir.x);
-			Mesh::PointID addPtsId[3];
-			addPtsId[0] = mesh->addPoint(pts[nbPts-1] + dir - dir2);
-			addPtsId[1] = mesh->addPoint(pts[nbPts-1]	    - dir2);
-			addPtsId[2] = mesh->addPoint(pts[nbPts-1] - dir - dir2);
-			UV.push_back(Point(1, 1));
-			UV.push_back(Point(1, 0.5));
-			UV.push_back(Point(1, 0));
+			case 2: // Square cap
+			{
+				Point dir2 = Point(dir.y, -dir.x);
+				Mesh::PointID addPtsId[3];
+				addPtsId[0] = mesh.addPoint(pts[nbPts-1] + dir - dir2);
+				addPtsId[1] = mesh.addPoint(pts[nbPts-1]	    - dir2);
+				addPtsId[2] = mesh.addPoint(pts[nbPts-1] - dir - dir2);
+				UV.push_back(Point(1, 1));
+				UV.push_back(Point(1, 0.5));
+				UV.push_back(Point(1, 0));
 
-			mesh->addTriangle(addPtsId[0], nextPtsId[0], addPtsId[1]);
-			mesh->addTriangle(nextPtsId[0], nextPtsId[1], addPtsId[1]);
-			mesh->addTriangle(addPtsId[1], nextPtsId[1], nextPtsId[2]);
-			mesh->addTriangle(addPtsId[1], nextPtsId[2], addPtsId[2]);
-			break;
-		}
+				mesh.addTriangle(addPtsId[0], nextPtsId[0], addPtsId[1]);
+				mesh.addTriangle(nextPtsId[0], nextPtsId[1], addPtsId[1]);
+				mesh.addTriangle(addPtsId[1], nextPtsId[1], nextPtsId[2]);
+				mesh.addTriangle(addPtsId[1], nextPtsId[2], addPtsId[2]);
+				break;
+			}
 		} // end switch
+
+
+	}
+
+	void update()
+	{
+		const auto& input = m_input.getValue();
+		const int nb = input.size();
+		auto output = m_output.getAccessor();
+		auto UV = m_coordUV.getAccessor();
+		output.clear();
+		output.resize(nb);
+		UV.clear();
+		UV.resize(nb);
+
+		for(int i=0; i<nb; ++i)
+			extrudePath(input[i], output[i], UV[i]);
 
 		cleanDirty();
 	}
 
 protected:
-	Data< Path > input;
-	Data< Animation<PReal> > width;
-	Data< int > capStyle, joinStyle;
-	Data< Mesh > output;
-	Data< QVector<Point> > coordUV;
+	Data< QVector<Path> > m_input;
+	Data< Animation<PReal> > m_width;
+	Data< int > m_capStyle, m_joinStyle;
+	Data< QVector<Mesh> > m_output;
+	Data< QVector<Path> > m_coordUV; // Hack: vector of vector of vec2
 };
 
 int PointListMath_ExtrudeClass = RegisterObject<PointListMath_Extrude>("Math/Path/Extrude")
