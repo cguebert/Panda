9256c68cb24bf583c8921c8f6fcee26b5e2f0e82
Christophe Guébert <christophe.guebert@gmail.com>
2015-12-29 17:43:01 +0100

    Added XmlDocument using TinyXML-2 to replace the Qt classes.

---
+++ core/panda/XmlDocument.cpp
@@ -0,0 +1,233 @@
+#include <panda/XmlDocument.h>
+
+#include <tinyxml2.h>
+
+namespace panda
+{
+
+XmlAttribute::XmlAttribute(const tinyxml2::XMLAttribute* attribute)
+	: m_attribute(attribute)
+{
+}
+
+XmlAttribute::operator bool() const
+{
+	return m_attribute != nullptr;
+}
+
+std::string XmlAttribute::name() const
+{
+	if (m_attribute)
+		return m_attribute->Name();
+	return "";
+}
+
+bool XmlAttribute::toBool() const
+{
+	if (m_attribute)
+		return m_attribute->BoolValue();
+	return false;
+}
+
+int XmlAttribute::toInt() const
+{
+	if (m_attribute)
+		return m_attribute->IntValue();
+	return 0;
+}
+
+unsigned int XmlAttribute::toUnsigned() const
+{
+	if (m_attribute)
+		return m_attribute->UnsignedValue();
+	return 0;
+}
+
+float XmlAttribute::toFloat() const
+{
+	if (m_attribute)
+		return m_attribute->FloatValue();
+	return 0;
+}
+
+double XmlAttribute::toDouble() const
+{
+	if (m_attribute)
+		return m_attribute->DoubleValue();
+	return 0;
+}
+
+std::string XmlAttribute::toString() const
+{
+	if (m_attribute)
+		return m_attribute->Value();
+	return "";
+}
+
+//****************************************************************************//
+
+XmlElement::XmlElement(tinyxml2::XMLElement* element)
+	: m_element(element)
+{
+}
+
+XmlElement::operator bool() const
+{
+	return m_element != nullptr;
+}
+
+std::string XmlElement::name() const
+{
+	if (m_element)
+		return m_element->Name();
+	return "";
+}
+
+void XmlElement::setName(const std::string& name)
+{
+	if (m_element)
+		m_element->SetName(name.c_str());
+}
+
+XmlElement XmlElement::firstChild(const std::string& name)
+{
+	if (m_element)
+		return XmlElement(m_element->FirstChildElement(name.c_str()));
+	return XmlElement();
+}
+
+XmlElement XmlElement::nextSibling(const std::string& name)
+{
+	if (m_element)
+		return XmlElement(m_element->NextSiblingElement(name.c_str()));
+	return XmlElement();
+}
+
+XmlElement XmlElement::addChild(const std::string& name)
+{
+	if (m_element)
+	{
+		auto newElement = m_element->GetDocument()->NewElement(name.c_str());
+		m_element->InsertEndChild(newElement);
+		return XmlElement(newElement);
+	}
+	return XmlElement();
+}
+
+void XmlElement::removeChild(XmlElement& child)
+{
+	if (m_element && child.m_element)
+	{
+		m_element->DeleteChild(child.m_element);
+		child.m_element = nullptr;
+	}
+}
+
+XmlAttribute XmlElement::attribute(const std::string& name) const
+{
+	if (m_element)
+		return XmlAttribute(const_cast<const tinyxml2::XMLElement*>(m_element)->FindAttribute(name.c_str()));
+	return XmlAttribute();
+}
+
+void XmlElement::removeAttribute(const std::string& name)
+{
+	if (m_element)
+		m_element->DeleteAttribute(name.c_str());
+}
+
+void XmlElement::setAttribute(const std::string& name, bool value)
+{
+	if (m_element)
+		m_element->SetAttribute(name.c_str(), value);
+}
+
+void XmlElement::setAttribute(const std::string& name, int value)
+{
+	if (m_element)
+		m_element->SetAttribute(name.c_str(), value);
+}
+
+void XmlElement::setAttribute(const std::string& name, unsigned int value)
+{
+	if (m_element)
+		m_element->SetAttribute(name.c_str(), value);
+}
+
+void XmlElement::setAttribute(const std::string& name, float value)
+{
+	if (m_element)
+		m_element->SetAttribute(name.c_str(), value);
+}
+
+void XmlElement::setAttribute(const std::string& name, double value)
+{
+	if (m_element)
+		m_element->SetAttribute(name.c_str(), value);
+}
+
+void XmlElement::setAttribute(const std::string& name, const std::string& value)
+{
+	if (m_element)
+		m_element->SetAttribute(name.c_str(), value.c_str());
+}
+
+std::string XmlElement::text() const
+{
+	if (m_element)
+		return m_element->GetText();
+	return "";
+}
+
+void XmlElement::setText(const std::string& text)
+{
+	if (m_element)
+		m_element->SetText(text.c_str());
+}
+
+//****************************************************************************//
+
+XmlDocument::XmlDocument()
+	: m_document(std::make_unique<tinyxml2::XMLDocument>())
+{
+}
+
+bool XmlDocument::loadFromFile(const std::string& path)
+{
+	return tinyxml2::XML_SUCCESS == m_document->LoadFile(path.c_str());
+}
+
+bool XmlDocument::loadFromMemory(const std::string& xmlContent)
+{
+	return tinyxml2::XML_SUCCESS == m_document->Parse(xmlContent.c_str(), xmlContent.size());
+}
+
+bool XmlDocument::saveToFile(const std::string& path)
+{
+	return tinyxml2::XML_SUCCESS == m_document->SaveFile(path.c_str());
+}
+
+std::string XmlDocument::saveToMemory()
+{
+	tinyxml2::XMLPrinter printer;
+	m_document->Print(&printer);
+	return printer.CStr();
+}
+
+XmlElement XmlDocument::root() const
+{
+	auto element = m_document->RootElement();
+	if (!element) // TODO: verify if document can have no root
+	{
+		element = m_document->NewElement("xml");
+		m_document->InsertEndChild(element);
+	}
+	return XmlElement(element);
+}
+
+void XmlDocument::clear()
+{
+	m_document->Clear();
+}
+
+} // namespace panda
---
+++ core/panda/XmlDocument.h
@@ -0,0 +1,90 @@
+#ifndef XMLDOCUMENT_H
+#define XMLDOCUMENT_H
+
+#include <panda/core.h>
+
+#include <memory>
+#include <string>
+
+namespace tinyxml2
+{
+class XMLAttribute;
+class XMLElement;
+class XMLDocument;
+}
+
+namespace panda
+{
+
+class PANDA_CORE_API XmlAttribute
+{
+public:
+	XmlAttribute(const tinyxml2::XMLAttribute* attribute = nullptr);
+
+	explicit operator bool() const; // Test if valid
+
+	std::string name() const;
+
+	bool toBool() const;
+	int toInt() const;
+	unsigned int toUnsigned() const;
+	float toFloat() const;
+	double toDouble() const;
+	std::string toString() const;
+
+private:
+	const tinyxml2::XMLAttribute* m_attribute = nullptr;
+};
+
+class PANDA_CORE_API XmlElement
+{
+public:
+	XmlElement(tinyxml2::XMLElement* element = nullptr);
+	explicit operator bool() const; // Test if valid
+
+	std::string name() const;
+	void setName(const std::string& name);
+
+	XmlElement firstChild(const std::string& name = "");
+	XmlElement nextSibling(const std::string& name = "");
+	XmlElement addChild(const std::string& name);
+	void removeChild(XmlElement& child);
+
+	XmlAttribute attribute(const std::string& name) const;
+	void removeAttribute(const std::string& name);
+
+	void setAttribute(const std::string& name, bool value);
+	void setAttribute(const std::string& name, int value);
+	void setAttribute(const std::string& name, unsigned int value);
+	void setAttribute(const std::string& name, float value);
+	void setAttribute(const std::string& name, double value);
+	void setAttribute(const std::string& name, const std::string& value);
+
+	std::string text() const;
+	void setText(const std::string& text);
+
+private:
+	tinyxml2::XMLElement* m_element = nullptr;
+};
+
+class PANDA_CORE_API XmlDocument
+{
+public:
+	XmlDocument();
+
+	bool loadFromFile(const std::string& path);
+	bool loadFromMemory(const std::string& xmlContent);
+
+	bool saveToFile(const std::string& path);
+	std::string saveToMemory();
+
+	XmlElement root() const;
+	void clear();
+
+private:
+	std::unique_ptr<tinyxml2::XMLDocument> m_document;
+};
+
+} // namespace panda
+
+#endif // XMLDOCUMENT_H
