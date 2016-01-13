d2a5a7aefff43630733174b9ac8cd2dde6e110e2
Christophe Guébert <christophe.guebert@gmail.com>
2016-01-13 17:06:20 +0100

    Added panda::UndoStack & UndoCommand.

---
+++ core/panda/UndoStack.cpp
@@ -0,0 +1,273 @@
+#include <panda/UndoStack.h>
+
+namespace panda
+{
+
+void UndoCommand::redo()
+{
+	for (auto& child : m_commandChilds)
+		child->redo();
+}
+
+void UndoCommand::undo()
+{
+	// Calling undo on childs, in reverse order
+	for (auto it = m_commandChilds.rbegin(), itEnd = m_commandChilds.rend(); it != itEnd; ++it)
+		(*it)->undo();
+}
+
+//****************************************************************************//
+
+void UndoStack::push(UndoCommand::SPtr command)
+{
+	command->redo();
+
+	bool inMacro = !m_macros.empty();
+
+	UndoCommand::SPtr current;
+	if (inMacro)
+	{
+		auto macro = m_macros.back();
+		if (!macro->m_commandChilds.empty())
+			current = macro->m_commandChilds.back();
+	}
+	else
+	{
+		if (m_index > 0)
+			current = m_commands[m_index - 1];
+		// Clear all undone commands after the current index
+		m_commands.erase(m_commands.begin() + m_index, m_commands.end());
+		if (m_cleanIndex > m_index) // Clean state has been removed
+			m_cleanIndex = -1;
+	}
+
+	bool tryToMerge = (current != nullptr)
+		&& (current->id() != -1)
+		&& (current->id() == command->id())
+		&& (inMacro || m_index != m_cleanIndex);
+
+	if (tryToMerge && current->mergeWith(command.get()))
+	{
+		if (!inMacro)
+		{
+			m_indexChangedSignal.run(m_index);
+			m_canUndoChangedSignal.run(canUndo());
+			m_undoTextChangedSignal.run(undoText());
+			m_canRedoChangedSignal.run(canRedo());
+			m_redoTextChangedSignal.run(redoText());
+		}
+	}
+	else
+	{
+		if (inMacro)
+			m_macros.back()->m_commandChilds.push_back(command);
+		else
+		{
+			m_commands.push_back(command);
+			checkStackLimit();
+			changeIndex(m_index + 1);
+		}
+	}
+}
+
+void UndoStack::clear()
+{
+	if (m_commands.empty())
+		return;
+
+	bool wasClean = isClean();
+
+	m_macros.clear();
+	m_commands.clear();
+	m_index = 0;
+	m_cleanIndex = 0;
+
+	m_indexChangedSignal.run(0);
+	m_canUndoChangedSignal.run(false);
+	m_undoTextChangedSignal.run("");
+	m_canRedoChangedSignal.run(false);
+	m_redoTextChangedSignal.run("");
+
+	if (!wasClean)
+		m_cleanChangedSignal.run(true);
+}
+
+void UndoStack::undo()
+{
+	if (!m_index)
+		return;
+
+	if (!m_macros.empty())
+		return;
+
+	int index = m_index - 1;
+	m_commands[index]->undo();
+	changeIndex(index + 1);
+}
+
+void UndoStack::redo()
+{
+	if (m_index == m_commands.size())
+		return;
+
+	if (!m_macros.empty())
+		return;
+
+	m_commands[m_index]->redo();
+	changeIndex(m_index + 1);
+}
+
+bool UndoStack::canUndo() const
+{
+	if (!m_macros.empty())
+		return false;
+	return m_index > 0;
+}
+
+bool UndoStack::canRedo() const
+{
+	if (!m_macros.empty())
+		return false;
+	return m_index < static_cast<int>(m_commands.size());
+}
+
+std::string UndoStack::undoText() const
+{
+	if (!m_macros.empty())
+		return "";
+	if (m_index > 0)
+		return m_commands[m_index - 1]->text();
+	return "";
+}
+
+std::string UndoStack::redoText() const
+{
+	if (!m_macros.empty())
+		return "";
+	if (m_index < static_cast<int>(m_commands.size()))
+		return m_commands[m_index]->text();
+	return "";
+}
+
+std::string UndoStack::text(int index) const
+{
+	if (index < 0 || index >= static_cast<int>(m_commands.size()))
+		return "";
+	return m_commands[index]->text();
+}
+
+void UndoStack::setClean()
+{
+	changeIndex(m_index, true);
+}
+
+bool UndoStack::isClean() const
+{
+	if (!m_macros.empty())
+		return false;
+	return m_cleanIndex == m_index;
+}
+
+void UndoStack::setUndoLimit(int limit)
+{ 
+	if (m_undoLimit == limit)
+		return;
+
+	m_undoLimit = limit; 
+	checkStackLimit();
+}
+
+void UndoStack::beginMacro(const std::string& text)
+{
+	auto cmd = std::make_shared<UndoCommand>();
+	cmd->setText(text);
+
+	if (m_macros.empty())
+	{
+		// Clear all undone commands after the current index
+		m_commands.erase(m_commands.begin() + m_index, m_commands.end());
+		if (m_cleanIndex > m_index)
+			m_cleanIndex = -1;
+		m_commands.push_back(cmd);
+	}
+	else
+		m_macros.back()->m_commandChilds.push_back(cmd);
+
+	m_macros.push_back(cmd);
+
+	if (m_macros.size() == 1)
+	{
+		m_canUndoChangedSignal.run(false);
+		m_undoTextChangedSignal.run("");
+		m_canRedoChangedSignal.run(false);
+		m_redoTextChangedSignal.run("");
+	}
+}
+
+void UndoStack::endMacro()
+{
+	m_macros.pop_back();
+	if (m_macros.empty())
+	{
+		checkStackLimit();
+		changeIndex(m_index + 1);
+	}
+}
+
+void UndoStack::setIndex(int index)
+{
+	if (index < 0)
+		index = 0;
+	int nbCommands = static_cast<int>(m_commands.size());
+	if (index > nbCommands)
+		index = nbCommands;
+
+	int i = m_index;
+	while (i < index)
+		m_commands[i++]->redo();
+	while (i > index)
+		m_commands[--i]->undo();
+
+	changeIndex(index);
+}
+
+void UndoStack::checkStackLimit()
+{
+	if (m_undoLimit <= 0 || !m_macros.empty() || m_undoLimit >= static_cast<int>(m_commands.size()))
+		return;
+
+	int delCount = m_commands.size() - m_undoLimit;
+	m_commands.erase(m_commands.begin(), m_commands.begin() + delCount);
+
+	m_index -= delCount;
+	if (m_cleanIndex != -1)
+	{
+		if (m_cleanIndex < delCount)
+			m_cleanIndex = -1; // Clean state has been removed
+		else
+			m_cleanIndex -= delCount;
+	}
+}
+
+void UndoStack::changeIndex(int index, bool clean)
+{
+	bool wasClean = (m_index == m_cleanIndex);
+	if (index != m_index)
+	{
+		m_index = index;
+		m_indexChangedSignal.run(index);
+		m_canUndoChangedSignal.run(canUndo());
+		m_undoTextChangedSignal.run(undoText());
+		m_canRedoChangedSignal.run(canRedo());
+		m_redoTextChangedSignal.run(redoText());
+	}
+
+	if (clean)
+		m_cleanIndex = m_index;
+
+	bool isClean = (m_index == m_cleanIndex);
+	if (isClean != wasClean)
+		m_cleanChangedSignal.run(isClean);
+}
+
+} // namespace panda
---
+++ core/panda/UndoStack.h
@@ -0,0 +1,109 @@
+#ifndef UNDOSTACK_H
+#define UNDOSTACK_H
+
+#include <panda/messaging.h>
+
+#include <deque>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace panda
+{
+
+class PANDA_CORE_API UndoCommand
+{
+public:
+	using SPtr = std::shared_ptr<UndoCommand>;
+
+	virtual void redo(); /// Applies the change. By default, calls redo() on all childs
+	virtual void undo(); /// Reverts the change. By default, calls undo() on all childs in reverse order
+
+	std::string text() const;
+	void setText(const std::string& text);
+
+	int id() const; /// If the command supports compression, this must return an integer unique to the command's class. Else return -1;
+	bool mergeWith(const UndoCommand* other); /// Attemps to merge this command with another of the same id.
+
+private:
+	friend class UndoStack;
+
+	std::string m_commandText;
+	std::vector<UndoCommand::SPtr> m_commandChilds;
+};
+
+class PANDA_CORE_API UndoStack
+{
+public:
+	void push(UndoCommand::SPtr command);
+	void clear();
+
+	void undo();
+	void redo();
+
+	bool canUndo() const;
+	bool canRedo() const;
+
+	std::string undoText() const;
+	std::string redoText() const;
+	std::string text(int index) const;
+
+	void setClean(); /// Marks the stack (and the current state) as clean.
+	bool isClean() const; /// Returns true if the last command is the clean state.
+
+	void setUndoLimit(int limit); /// When the number of commands on the stack is higher than the limit, commands are remooved from the bottom.
+	int undoLimit() const;
+
+	void beginMacro(const std::string& text); /// Any following commands until a call to endMacro() will be treated as one single command 
+	void endMacro();
+
+	int count() const;
+	int index() const;
+	int cleanIndex() const;
+	void setIndex(int index);
+
+// Signals
+	msg::Signal<void(bool)> m_canUndoChangedSignal;
+	msg::Signal<void(bool)> m_canRedoChangedSignal;
+	msg::Signal<void(int)> m_indexChangedSignal;
+	msg::Signal<void(bool)> m_cleanChangedSignal;
+	msg::Signal<void(const std::string&)> m_undoTextChangedSignal;
+	msg::Signal<void(const std::string&)> m_redoTextChangedSignal;
+
+private:
+	void checkStackLimit();
+	void changeIndex(int index, bool clean = false);
+
+	int m_index = 0, m_cleanIndex = 0, m_undoLimit = 0;
+	std::deque<UndoCommand::SPtr> m_commands, m_macros;
+};
+
+//****************************************************************************//
+
+inline std::string UndoCommand::text() const
+{ return m_commandText; }
+
+inline void UndoCommand::setText(const std::string& text)
+{ m_commandText = text; }
+
+inline int UndoCommand::id() const
+{ return -1; }
+
+inline bool UndoCommand::mergeWith(const UndoCommand* other)
+{ return false; }
+
+inline int UndoStack::undoLimit() const
+{ return m_undoLimit; }
+
+inline int UndoStack::count() const
+{ return m_commands.size(); }
+
+inline int UndoStack::index() const
+{ return m_index; }
+
+inline int UndoStack::cleanIndex() const
+{ return m_cleanIndex; }
+
+} // namespace panda
+
+#endif // UNDOSTACK_H
