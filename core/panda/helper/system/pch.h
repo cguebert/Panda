ce3c43abbbfbf7f89ed9eace94466e01f2af467a
Christophe Guébert <christophe.guebert@gmail.com>
2016-01-16 18:09:11 +0100

    Readded the precompiled header.

---
+++ core/panda/helper/system/pch.h
@@ -0,0 +1,33 @@
+#include <panda/core.h>
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <functional>
+#include <limits>
+#include <map>
+#include <memory>
+#include <set>
+#include <stdint.h>
+#include <string>
+#include <typeinfo>
+#include <vector>
+
+#include <panda/types/Animation.h>
+#include <panda/types/Color.h>
+#include <panda/types/Gradient.h>
+#include <panda/types/ImageWrapper.h>
+#include <panda/types/Mesh.h>
+#include <panda/types/Polygon.h>
+#include <panda/types/Rect.h>
+#include <panda/types/Shader.h>
+
+#include <panda/graphics/Framebuffer.h>
+#include <panda/graphics/ShaderProgram.h>
+
+#include <panda/data/Data.h>
+#include <panda/object/PandaObject.h>
+#include <panda/PandaDocument.h>
