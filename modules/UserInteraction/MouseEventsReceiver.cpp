123b06914020ed051a931a94474a692fa3d21411
Christophe Guébert <christophe.guebert@gmail.com>
2016-02-26 18:45:12 +0100

    Modified the objects of the UserInteraction module so that clicks are not registered when the animation is not playing.

--- modules/UserInteraction/MouseEventsReceiver.cpp
+++ modules/UserInteraction/MouseEventsReceiver.cpp
@@ -8,9 +8,22 @@ namespace panda
 {
 
 MouseEventsReceiver::MouseEventsReceiver(PandaDocument* doc)
+	: m_document(doc)
 {
-	m_observer.get(doc->getSignals().mousePressed).connect<MouseEventsReceiver, &MouseEventsReceiver::mousePressed>(this);
-	m_observer.get(doc->getSignals().mouseReleased).connect<MouseEventsReceiver, &MouseEventsReceiver::mouseReleased>(this);
+	m_observer.get(doc->getSignals().mousePressed).connect<MouseEventsReceiver, &MouseEventsReceiver::onMousePressed>(this);
+	m_observer.get(doc->getSignals().mouseReleased).connect<MouseEventsReceiver, &MouseEventsReceiver::onMouseReleased>(this);
+}
+
+void MouseEventsReceiver::onMousePressed(panda::types::Point pt)
+{
+	if (m_document->animationIsPlaying())
+		mousePressed(pt);
+}
+
+void MouseEventsReceiver::onMouseReleased(panda::types::Point pt)
+{
+	if (m_document->animationIsPlaying())
+		mouseReleased(pt);
 }
 
 }
--- modules/UserInteraction/MouseEventsReceiver.h
+++ modules/UserInteraction/MouseEventsReceiver.h
@@ -8,14 +8,18 @@ using types::Point;
 
 class MouseEventsReceiver
 {
-public:
+protected:
 	MouseEventsReceiver(PandaDocument* doc);
 
 	virtual void mousePressed(panda::types::Point) {}
 	virtual void mouseReleased(panda::types::Point) {}
 
-protected:
+private:
 	msg::Observer m_observer;
+	PandaDocument* m_document;
+
+	void onMousePressed(panda::types::Point);
+	void onMouseReleased(panda::types::Point);
 };
 
 } // namespace Panda
