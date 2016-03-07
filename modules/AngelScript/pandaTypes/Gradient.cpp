e6e2e08e797d89e0f6f5d424f5b6a06f96d303d5
Christophe Guébert <christophe.guebert@gmail.com>
2016-03-07 18:59:43 +0100

    WIP code for the use of Gradients vector in AngelScript.

--- modules/AngelScript/pandaTypes/Gradient.cpp
+++ modules/AngelScript/pandaTypes/Gradient.cpp
@@ -17,6 +17,9 @@ namespace
 {
 	BaseDataWrapper* createGradientData(bool input, const std::string& name, const std::string& help, ObjectWrapper* wrapper)
 	{ return wrapper->createData<Gradient>(input, name, help); }
+
+	BaseDataWrapper* createGradientVectorData(bool input, const std::string& name, const std::string& help, ObjectWrapper* wrapper)
+	{ return wrapper->createData<std::vector<Gradient>>(input, name, help); }
 }
 
 namespace panda 
@@ -36,8 +39,7 @@ namespace panda
 		GradientWrapper(const panda::types::Gradient& gradient)
 			: m_gradient(gradient) {}
 
-		const panda::types::Gradient& gradient() const
-		{ return m_gradient; }
+		const panda::types::Gradient& gradient() const { return m_gradient; }
 
 		void addRef() { m_refCount++; }
 		void release() { if (--m_refCount == 0) delete this; }
@@ -46,16 +48,18 @@ namespace panda
 		void add(float pos, panda::types::Color col) { m_gradient.add(pos, col); }
 		panda::types::Color get(float pos) { return m_gradient.get(pos); }
 
+		static GradientWrapper& Assign(GradientWrapper* other, GradientWrapper* self)
+		{ return *self = *other; }
+
 	private:
 		panda::types::Gradient m_gradient;
 		int m_refCount = 1;
 	};
 
-	template <>
-	class DataWrapper<types::Gradient, GradientWrapper*> : public BaseDataWrapper
+	class GradientDataWrapper : public BaseDataWrapper
 	{
 	public:
-		DataWrapper(Data<types::Gradient>* data)
+		GradientDataWrapper(Data<types::Gradient>* data)
 			: m_data(data) { }
 
 		GradientWrapper* getValue() const
@@ -71,17 +75,64 @@ namespace panda
 		Data<types::Gradient>* m_data = nullptr;
 	};
 
+	class GradientVectorDataWrapper : public BaseDataWrapper
+	{
+	public:
+		using value_type = std::vector<Gradient>;
+		using data_type = Data<value_type>;
+		using script_vector = aatc::container::tempspec::vector<GradientWrapper*>;
+
+		GradientVectorDataWrapper(data_type* data)
+			: m_data(data) { }
+
+		script_vector* getValue() const
+		{
+			auto* vec = new script_vector();
+			const auto& gradients = m_data->getValue();
+			for (const auto& gradient : gradients)
+				vec->container.push_back(GradientWrapper::create(gradient));
+			return vec;
+		}
+	
+		void setValue(script_vector* vec)
+		{
+			auto acc = m_data->getAccessor();
+			acc.clear();
+			acc.reserve(vec->container.size());
+			for (const auto& wrapper : vec->container)
+				acc.push_back(wrapper->gradient());
+		}
+
+		int getCounter() const 
+		{ return m_data->getCounter(); }
+	
+	private:
+		data_type* m_data = nullptr;
+	};
+
+
 	void registerGradientData(asIScriptEngine* engine)
 	{
-		using WrapperType = DataWrapper<Gradient, GradientWrapper*>;
 		int r = 0;
 		r = engine->RegisterObjectType("Data<Gradient>", 0, asOBJ_REF | asOBJ_NOCOUNT); assert(r >= 0);
 		r = engine->RegisterObjectMethod("Data<Gradient>", "Gradient@ getValue()",
-			asMETHOD(WrapperType, getValue), asCALL_THISCALL); assert(r >= 0);
+			asMETHOD(GradientDataWrapper, getValue), asCALL_THISCALL); assert(r >= 0);
 		r = engine->RegisterObjectMethod("Data<Gradient>", "void setValue(Gradient@)",
-			asMETHOD(WrapperType, setValue), asCALL_THISCALL); assert(r >= 0);
+			asMETHOD(GradientDataWrapper, setValue), asCALL_THISCALL); assert(r >= 0);
 		r = engine->RegisterObjectMethod("Data<Gradient>", "int getCounter()",
-			asMETHOD(WrapperType, getCounter), asCALL_THISCALL); assert(r >= 0);
+			asMETHOD(GradientDataWrapper, getCounter), asCALL_THISCALL); assert(r >= 0);
+	}
+
+	void registerGradientVectorDataType(asIScriptEngine* engine)
+	{
+		int r = 0;
+		r = engine->RegisterObjectType("Data<vector<Gradient@>>", 0, asOBJ_REF | asOBJ_NOCOUNT); assert(r >= 0);
+		r = engine->RegisterObjectMethod("Data<vector<Gradient@>>", str("vector<Gradient@>@ getValue()"),
+			asMETHOD(GradientVectorDataWrapper, getValue), asCALL_THISCALL); assert(r >= 0);
+		r = engine->RegisterObjectMethod("Data<vector<Gradient@>>", str("void setValue(vector<Gradient@>@)"),
+			asMETHOD(GradientVectorDataWrapper, setValue), asCALL_THISCALL); assert(r >= 0);
+		r = engine->RegisterObjectMethod("Data<vector<Gradient@>>", "int getCounter()",
+			asMETHOD(GradientVectorDataWrapper, getCounter), asCALL_THISCALL); assert(r >= 0);
 	}
 
 	void registerGradientType(asIScriptEngine* engine)
@@ -92,6 +143,7 @@ namespace panda
 
 		r = engine->RegisterObjectBehaviour("Gradient", asBEHAVE_ADDREF, "void f()", asMETHOD(GradientWrapper, addRef), asCALL_THISCALL); assert( r >= 0 );
 		r = engine->RegisterObjectBehaviour("Gradient", asBEHAVE_RELEASE, "void f()", asMETHOD(GradientWrapper, release), asCALL_THISCALL); assert( r >= 0 );
+		r = engine->RegisterObjectMethod("Gradient", "Gradient& opAssign(Gradient &in)", asFUNCTION(GradientWrapper::Assign), asCALL_CDECL_OBJLAST); assert( r >= 0 );
 
 		r = engine->RegisterObjectMethod("Gradient", "void clear()", asMETHOD(GradientWrapper, clear), asCALL_THISCALL); assert( r >= 0 );
 		r = engine->RegisterObjectMethod("Gradient", "void add(float, Color)", asMETHOD(GradientWrapper, add), asCALL_THISCALL); assert( r >= 0 );
@@ -102,8 +154,14 @@ namespace panda
 	{
 		registerGradientType(engine);
 		registerGradientData(engine);
+
+		aatc::container::tempspec::vector<GradientWrapper*>::Register(engine, "Gradient@");
+		registerGradientVectorDataType(engine);
+
 		int r = engine->RegisterObjectMethod("PandaObject", "Data<Gradient>@ createGradientData(bool, const string &in, const string &in)",
 			asFUNCTION(createGradientData), asCALL_CDECL_OBJLAST); assert(r >= 0);
+		engine->RegisterObjectMethod("PandaObject", "Data<vector<Gradient@>>@ createGradientVectorData(bool, const string &in, const string &in)",
+			asFUNCTION(createGradientVectorData), asCALL_CDECL_OBJLAST); assert(r >= 0);
 	}
 
 } // namespace panda
