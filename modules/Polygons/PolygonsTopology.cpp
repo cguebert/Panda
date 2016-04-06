ad3bdf6e669efbf5b92f2459adbdab8e2d662792
Christophe Guébert <christophe.guebert@gmail.com>
2016-04-06 22:02:36 +0200

    Added the object FacesAroundPoint for polygons.

---
+++ modules/Polygons/PolygonsTopology.cpp
@@ -0,0 +1,72 @@
+#include <panda/object/PandaObject.h>
+#include <panda/object/ObjectFactory.h>
+#include <panda/types/IntVector.h>
+#include <panda/types/Path.h>
+
+#include <algorithm>
+
+namespace panda {
+
+using types::IntVector;
+using types::Path;
+using types::Point;
+
+class Polygon_FacesAroundPoint : public PandaObject
+{
+public:
+	PANDA_CLASS(Polygon_FacesAroundPoint, PandaObject)
+
+	Polygon_FacesAroundPoint(PandaDocument *doc)
+		: PandaObject(doc)
+		, m_indices(initData("indices", "Indices of points forming the polygons"))
+		, m_neighbors(initData("neighbors", "For each point, neighboring polygons"))
+	{
+		addInput(m_indices);
+		addOutput(m_neighbors);
+	}
+
+	void update()
+	{
+		const auto& indices = m_indices.getValue();
+		auto neighborsAcc = m_neighbors.getAccessor();
+		auto& neighbors = neighborsAcc.wref();
+		neighbors.clear();
+
+		// First pass, get the number of points
+		int nbPoints = -1;
+		for (const auto& list : indices)
+		{
+			for (const auto& id : list.values)
+			{
+				if (id > nbPoints)
+					nbPoints = id;
+			}
+		}
+
+		if (nbPoints < 0)
+			return;
+
+		// Second pass, add the index of each polygon to every of its points
+		neighbors.resize(nbPoints + 1);
+		int nbPoly = indices.size();
+		for (int i = 0; i < nbPoly; ++i)
+		{
+			const auto& poly = indices[i];
+			for (const auto& id : poly.values)
+				neighbors[id].values.push_back(i);
+		}
+	}
+
+protected:
+	Data< std::vector<IntVector> > m_indices, m_neighbors;	
+};
+
+int Polygon_FacesAroundPointClass = RegisterObject<Polygon_FacesAroundPoint>("Generator/Polygon/Polygons around point")
+	.setDescription("Compute the neighboring polygon of each point");
+
+//****************************************************************************//
+
+
+} // namespace Panda
+
+
