c927b40c202be886abc262774ae463eab725c5ae
Christophe Guébert <christophe.guebert@gmail.com>
2016-04-07 15:21:23 +0200

    Reworked the object ListCondition so the behavior when the inputs are of different lengths is similar to the rest of Panda.

--- modules/List/Condition.cpp
+++ modules/List/Condition.cpp
@@ -58,20 +58,24 @@ public:
 
 		const auto& inT = dataInTrue->getValue();
 		const auto& inF = dataInFalse->getValue();
-		int nb = std::min(inT.size(), inF.size());
+		int nbT = inT.size(), nbF = inF.size();
+		int nb = std::max(nbT, nbF);
 		if(nbC == 1 || nbC < nb)
 			outVal = c[0] ? inT : inF;
-		else if(nb == 1)
+		else if (nb == 1)
 		{
 			outVal.resize(nbC);
-			for(int i=0; i<nbC; ++i)
+			for (int i = 0; i < nbC; ++i)
 				outVal[i] = c[i] ? inT[0] : inF[0];
 		}
-		else // nbC == nb
+		else
 		{
+			if (nbT < nb) nbT = 1;
+			if (nbF < nb) nbF = 1;
+
 			outVal.resize(nb);
 			for(int i=0; i<nb; ++i)
-				outVal[i] = c[i] ? inT[i] : inF[i];
+				outVal[i] = c[i] ? inT[i % nbT] : inF[i % nbF];
 		}
 	}
 
