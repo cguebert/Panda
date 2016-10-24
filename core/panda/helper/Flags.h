ec82d13c541d8250f574ccf80f56055074dc0d5d
Christophe Guébert <christophe.guebert@gmail.com>
2016-10-24 21:28:16 +0200

    Simplified and fixed the compilation of Flags.

--- core/panda/helper/Flags.h
+++ core/panda/helper/Flags.h
@@ -10,35 +10,35 @@ namespace helper
 	class Flags
 	{
 	public:
-		using underlying = std::underlying_type_t<E>;
+		using underlying = std::underlying_type_t<Enum>;
 		using enum_type = Enum;
 
-		constexpr inline Flags() : m_value(0) {}
-		constexpr inline Flags(Enum v) : m_value(static_cast<underlying>(v)) {}
-		constexpr inline Flags(underlying v) : m_value(v) {}
+		Flags() : m_value(0) {}
+		Flags(Enum v) : m_value(static_cast<underlying>(v)) {}
+		Flags(underlying v) : m_value(v) {}
 
-		constexpr inline Flags &operator&=(Enum mask) { m_value &= static_cast<underlying>(mask); return *this; }
-		constexpr inline Flags &operator|=(Flags v) { m_value |= v.m_value; return *this; }
-		constexpr inline Flags &operator|=(Enum v) { m_value |= static_cast<underlying>(v); return *this; }
-		constexpr inline Flags &operator^=(Flags v) { m_value ^= v.m_value; return *this; }
-		constexpr inline Flags &operator^=(Enum v) { m_value ^= static_cast<underlying>(v); return *this; }
+		Flags &operator&=(Enum mask) { m_value &= static_cast<underlying>(mask); return *this; }
+		Flags &operator|=(Flags v) { m_value |= v.m_value; return *this; }
+		Flags &operator|=(Enum v) { m_value |= static_cast<underlying>(v); return *this; }
+		Flags &operator^=(Flags v) { m_value ^= v.m_value; return *this; }
+		Flags &operator^=(Enum v) { m_value ^= static_cast<underlying>(v); return *this; }
 
-		constexpr inline operator underlying() const { return m_value; }
-		constexpr inline operator bool() const { return m_value != 0; }
+		operator underlying() const { return m_value; }
+		operator bool() const { return m_value != 0; }
 
-		constexpr inline operator==(Enum v) const { return m_value == tatic_cast<underlying>(v); }
+		bool operator==(Enum v) const { return m_value == static_cast<underlying>(v); }
 
-		constexpr inline Flags operator|(Flags v) const { return Flags(m_value | v.m_value); }
-		constexpr inline Flags operator|(Enum v) const { return Flags(m_value | static_cast<underlying>(v)); }
-		constexpr inline Flags operator^(Flags v) const { return Flags(m_value ^ v.m_value); }
-		constexpr inline Flags operator^(Enum v) const { return Flags(m_value ^ static_cast<underlying>(v)); }
-		constexpr inline Flags operator&(underlying mask) const { return Flags(m_value & mask); }
-		constexpr inline Flags operator&(Enum v) const { return Flags(m_value & static_cast<underlying>(v)); }
-		constexpr inline Flags operator~() const { return Flags(~m_value); }
+		Flags operator|(Flags v) const { return Flags(m_value | v.m_value); }
+		Flags operator|(Enum v) const { return Flags(m_value | static_cast<underlying>(v)); }
+		Flags operator^(Flags v) const { return Flags(m_value ^ v.m_value); }
+		Flags operator^(Enum v) const { return Flags(m_value ^ static_cast<underlying>(v)); }
+		Flags operator&(underlying mask) const { return Flags(m_value & mask); }
+		Flags operator&(Enum v) const { return Flags(m_value & static_cast<underlying>(v)); }
+		Flags operator~() const { return Flags(~m_value); }
 
-		constexpr inline bool operator!() const { return !m_value; }
+		bool operator!() const { return !m_value; }
 
-		constexpr inline bool has(Enum v) const 
+		bool has(Enum v) const 
 		{
 			const auto val = static_cast<underlying>(v);
 			return (m_value & val) == val && (val != 0 || m_value == val); 
